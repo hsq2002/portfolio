4e5bde236c39b433a0a2849ca690b181031eac20
Shehzad938 <111834197+hsq2002@users.noreply.github.com>
2024-10-04 23:54:56 -0500

    text-boxes

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 
 const Grid = () => {
-  return <div>Grid</div>;
-};
+  return (
+  <section id="about">
+    <BentoGrid>
+      {gridItems.map(({id, title, description, className,
+          img, imgClassName, titleClassName, spareImg, test
+      }) => (
+        <BentoGridItem
+        id={id}
+        key={id}
+        title={title}
+        description={description}
+        className={className} //classNames in gridItems
+                                    //array get called and apply tailwind
+        img={img}
+        imgClassName = {imgClassName}
+        titleClassName = {titleClassName}
+        spareImg = {spareImg}
+        />
+
+      ))}
+    </BentoGrid>
+  </section>
+)};
 
 export default Grid;
